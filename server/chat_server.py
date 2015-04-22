0c23ed65109830b1cb34fa3d633f2b4b0b5cd534
mrreload <marc@thehoaglins.com>
2015-04-21 22:04:52 -0700

    chat client thread q working. now need to setup server to send data

--- server/chat_server.py
+++ server/chat_server.py
@@ -6,68 +6,76 @@ import socket, select, sys, traceback
 ms = __import__('messageserv')
 cfg = __import__('config')
 
-# Function to broadcast chat messages to all connected clients
-def broadcast_data(message):
-	#Do not send the message to master socket and the client who has send us the message
-	for socket in CONNECTION_LIST:
-		if socket != server_socket and socket != sock:
-			try:
-				socket.send(message)
-			except:
-				# broken socket connection may be, chat client pressed ctrl+c for example
-				socket.close()
-				CONNECTION_LIST.remove(socket)
+
+class msg_server(object):
+	def __init__(self):
+		# List to keep track of socket descriptors
+		self.CONNECTION_LIST = []
+		self.RECV_BUFFER = 4096  # Advisable to keep it as an exponent of 2
+		self.PORT = cfg.msg_port
+
+	# Function to broadcast chat messages to all connected clients
+	def broadcast_data(self, message):
+		# Do not send the message to master socket and the client who has send us the message
+		for socket in self.CONNECTION_LIST:
+			if socket != server_socket and socket != sock:
+				try:
+					socket.send(message)
+				except:
+					# broken socket connection may be, chat client pressed ctrl+c for example
+					socket.close()
+					self.CONNECTION_LIST.remove(socket)
 
 
-if __name__ == "__main__":
-	mserv = ms.MessageServ()
-	# List to keep track of socket descriptors
-	CONNECTION_LIST = []
-	RECV_BUFFER = 4096  # Advisable to keep it as an exponent of 2
-	PORT = cfg.msg_port
+	def nothuman(self):
+		mserv = ms.MessageServ()
 
-	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	# this has no effect, why ?
-	server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	server_socket.bind(("0.0.0.0", PORT))
-	server_socket.listen(10)
+		server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		# this has no effect, why ?
+		server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		server_socket.bind(("0.0.0.0", self.PORT))
+		server_socket.listen(10)
 
-	# Add server socket to the list of readable connections
-	CONNECTION_LIST.append(server_socket)
+		# Add server socket to the list of readable connections
+		self.CONNECTION_LIST.append(server_socket)
 
-	print "Chat server started on port " + str(PORT)
+		print "Chat server started on port " + str(self.PORT)
 
-	while 1:
-		# Get the list sockets which are ready to be read through select
-		read_sockets, write_sockets, error_sockets = select.select(CONNECTION_LIST, [], [])
+		while 1:
+			# Get the list sockets which are ready to be read through select
+			read_sockets, write_sockets, error_sockets = select.select(self.CONNECTION_LIST, [], [])
 
-		for sock in read_sockets:
-			#New connection
-			if sock == server_socket:
-				# Handle the case in which there is a new connection received through server_socket
-				sockfd, addr = server_socket.accept()
-				CONNECTION_LIST.append(sockfd)
-				print "Client (%s, %s) connected" % addr
+			for sock in read_sockets:
+				# New connection
+				if sock == server_socket:
+					# Handle the case in which there is a new connection received through server_socket
+					sockfd, addr = server_socket.accept()
+					self.CONNECTION_LIST.append(sockfd)
+					print "Client (%s, %s) connected" % addr
 
-				broadcast_data("[%s:%s] entered room\n" % addr)
+					broadcast_data("[%s:%s] entered room\n" % addr)
 
-			#Some incoming message from a client
-			else:
-				# Data received from client, process it
-				#try:
+				#Some incoming message from a client
+				else:
+					# Data received from client, process it
+					#try:
 					#In Windows, sometimes when a TCP program closes abruptly,
 					# a "Connection reset by peer" exception will be thrown
-				data = sock.recv(RECV_BUFFER)
-				if data:
-					#broadcast_data("\r" + '<' + str(sock.getpeername()) + '> ' + data)
-					mserv.read_data(data)
+					data = sock.recv(self.RECV_BUFFER)
+					if data:
+						#broadcast_data("\r" + '<' + str(sock.getpeername()) + '> ' + data)
+						mserv.read_data(data)
 
-				#except:
-				#	print "Unexpected error:", sys.exc_info()[0]
-				#	broadcast_data("Client (%s, %s) is offline" % addr)
-				#	print "Client (%s, %s) is offline" % addr
-				#	sock.close()
-				#	CONNECTION_LIST.remove(sock)
-				#	continue
+					#except:
+					#	print "Unexpected error:", sys.exc_info()[0]
+					#	broadcast_data("Client (%s, %s) is offline" % addr)
+					#	print "Client (%s, %s) is offline" % addr
+					#	sock.close()
+					#	CONNECTION_LIST.remove(sock)
+					#	continue
 
-	server_socket.close()
+		server_socket.close()
+
+if __name__ == "__main__":
+	m = msg_server()
+	m.nothuman()
--- server/pantilt.py
+++ server/pantilt.py
@@ -8,7 +8,7 @@ servo = PWM(0x40)
 servo.setPWMFreq(50)
 
 TILT = 0
-PAN  = 3
+PAN = 3
 
 pan_center = 90
 tilt_center = 50
@@ -21,44 +21,42 @@ servo_tilt_down = 465
 
 def pan(deg):
 	print("Pan " + str(deg))
-  	#cali(PAN,deg)
-	pwm = getPWM(deg,pan_per_deg,servo_pan_left)
-	print(str(pwm))
-	servo.setPWM(PAN, 0, getPWM(deg,pan_per_deg,servo_pan_left))
+	# cali(PAN,deg)
+	# pwm = getPWM(deg, pan_per_deg, servo_pan_left)
+	# print(str(pwm))
+	servo.setPWM(PAN, 0, getPWM(deg, pan_per_deg, servo_pan_left))
+
 def tilt(deg):
 	print("Tilt " + str(deg))
-	_turn2(TILT,deg)
+	# _turn2(TILT,deg)
+	servo.setPWM(TILT, 0, getPWM(deg, tilt_per_deg, servo_tilt_up))
 
-def _turn(s,deg):
-  	pwm = 451.0 + ((deg/180.0) * 1000.0)
 
-  	pwm = (4096.0/20000.0) * pwm
-  	pwm = int(pwm)
-  	print(str(pwm))
+def _turn(s, deg):
+	pwm = 451.0 + ((deg / 180.0) * 1000.0)
+	pwm = (4096.0 / 20000.0) * pwm
+	pwm = int(pwm)
+	print(str(pwm))
+	servo.setPWM(s, 0, pwm)
 
-  	servo.setPWM(s, 0, pwm)
 
-def _turn2(s,deg):
-  	pwm = (float(deg) * tilt_per_deg) + float(servo_tilt_up)
-  	print("Moving: " + str(pwm))
-  	pwm = int(pwm)
-  	servo.setPWM(s, 0, pwm)
+def _turn2(s, deg):
+	pwm = (float(deg) * tilt_per_deg) + float(servo_tilt_up)
+	print("Moving: " + str(pwm))
+	pwm = int(pwm)
+	servo.setPWM(s, 0, pwm)
 
 def reset():
 	pan(pan_center)
 	tilt(tilt_center)
-	
-def _turn3(s,deg):
-  	pwm = (float(deg) * pan_per_deg) + float(servo_pan_left)
-  	print("Moving: " + str(pwm))
-  	pwm = int(pwm)
-  	servo.setPWM(s, 0, pwm)
 
-def cali(s,pnt):
+
+def cali(s, pnt):
 	print("Move to: " + str(pnt))
 	servo.setPWM(s, 0, pnt)
 
-def getPWM(deg,offset,srv_min):
+
+def getPWM(deg, offset, srv_min):
 	pwm = (float(deg) * offset) + float(srv_min)
 	return int(pwm)
 
